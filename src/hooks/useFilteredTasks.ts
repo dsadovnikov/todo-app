fb0c1939a66b6f0a02e4014b9438edebf1447607
Denis Sadovnikov <den.sadovnikov@gmail.com>
2022-06-11 18:32:49 +0300

    Fixed some bugs, added memoization

--- src/hooks/useFilteredTasks.ts
+++ src/hooks/useFilteredTasks.ts
@@ -1,14 +1,19 @@
+import { useMemo } from 'react';
 import { ITask, TaskStatusFilterType } from '../types/Task';
 
 export const useFilteredTasks = (tasks: ITask[], filter: string) => {
-  switch (filter) {
-    case TaskStatusFilterType.all:
-      return tasks;
-    case TaskStatusFilterType.active:
-      return tasks.filter((tasks) => tasks.isComplete === false);
-    case TaskStatusFilterType.completed:
-      return tasks.filter((tasks) => tasks.isComplete === true);
-    default:
-      return tasks;
-  }
+  const filteredTasks = useMemo(() => {
+    switch (filter) {
+      case TaskStatusFilterType.all:
+        return tasks;
+      case TaskStatusFilterType.active:
+        return tasks.filter((tasks) => tasks.isComplete === false);
+      case TaskStatusFilterType.completed:
+        return tasks.filter((tasks) => tasks.isComplete === true);
+      default:
+        return tasks;
+    }
+  }, [tasks, filter]);
+
+  return filteredTasks;
 };
